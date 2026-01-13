356a515c4d9a0c51baad0c825837725a4b5bd361
Raihan <113172842+Raihanroo@users.noreply.github.com>
2026-01-13 15:39:22 +0600

    removed venv folder tracked files

--- my_project_mkdir/my_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Message
-
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ("name", "email", "created_at")  # লিস্টে এগুলো দেখাবে
-    list_filter = ("created_at",)  # তারিখ অনুযায়ী ফিল্টার
-    search_fields = ("name", "email", "message")  # সার্চ করতে পারবে
-    readonly_fields = ("created_at",)  # created_at এডিট করা যাবে না
