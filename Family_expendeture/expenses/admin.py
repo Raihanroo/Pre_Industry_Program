64b312c0dac0e140266b2b5fc8307bf118ee754a
Raihan <raihanroo21@gmail.com>
2026-03-03 15:46:15 +0600

    changed income source to text input and removed family head

--- Family_expendeture/expenses/admin.py
+++ Family_expendeture/expenses/admin.py
@@ -1,14 +1,13 @@
-from django.contrib import admin
 
 from django.contrib import admin
-from .models import IncomeSource, ExpenseCategory, FamilyMember, Expenditure, Budget, SavingsGoal, ActivityLog
+from .models import ExpenseCategory, FamilyMember, Expenditure, Budget, SavingsGoal, ActivityLog
 
 # নতুন মডেলগুলো রেজিস্টার করুন
 admin.site.register(ExpenseCategory)
 admin.site.register(FamilyMember)
 admin.site.register(Expenditure)
 
-# আগের গুলো যদি অলরেডি রেজিস্টার করা না থাকে
+# # আগের গুলো যদি অলরেডি রেজিস্টার করা না থাকে
 # admin.site.register(Budget)
 # admin.site.register(SavingsGoal)
 # admin.site.register(ActivityLog)
